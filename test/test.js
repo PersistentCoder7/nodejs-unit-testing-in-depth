da903489eabb6f00736ba621ea0887e3be17fa46
Prabhugurudarshan Singh <prabhu25.1975@gmail.com>
2022-01-22 17:56:51 +0530

    The before and after work for only a specific context if they are defined within the context. Also, each describe block can have multiple context blocks within it

--- test/test.js
+++ test/test.js
@@ -1,24 +1,27 @@
 const assert = require('assert');
 
 describe('file to be tested', () => {
-    //These functions are executed before executing any test and after completing all the tests.
-    before(() => {
-        console.log('===before');
-    });
-
-    after(() => {
-        console.log('===after');
-    });
-
-    //These test are executed before and after each 'it' block of the test is executed.
-    beforeEach(() => {
-        console.log('===before each');
-    });
-
-    afterEach(() => {
-        console.log('===after each');
-    });
+   
     context('function to be tested', () => {
+         //These functions are executed before executing any test and after completing all the tests.
+        before(() => {
+            console.log('===before');
+        });
+
+        after(() => {
+            console.log('===after');
+        });
+
+        //These test are executed before and after each 'it' block of the test is executed.
+        beforeEach(() => {
+            console.log('===before each');
+        });
+
+        afterEach(() => {
+            console.log('===after each');
+        });
+
+
         it('should do something', () => {
             assert.equal(1,1);
         });
@@ -27,5 +30,9 @@ describe('file to be tested', () => {
             assert.deepEqual({a:1, b:2},{a:1, b:2});
         });
         it('this is a pending test');
+    });
+
+    context('another function', () => {
+        it('test a new function within the new context');
     });
 });
